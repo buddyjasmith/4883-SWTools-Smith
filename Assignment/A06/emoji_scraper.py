827dc967f18568ab19bf80a0450926b6b897ea5d
Drew Smith <BuddyJASmith@gmail.com>
2019-03-08 16:10:26 -0600

    Upload Program

--- Assignment/A06/emoji_scraper.py
+++ Assignment/A06/emoji_scraper.py
@@ -1,17 +1,63 @@
+'''
+Name: Buddy Smith
+Class: 4883_Software_Tools
+Desc: The program scrapes emojis from webfx.com/tools/emoji-cheat-sheet and
+      stores the content and zips the file for easy uploading time.
+'''
 from beautifulscraper import BeautifulScraper
-from pprint import pprint
-from bs4 import BeautifulSoup
+from zipfile import ZipFile
 import os
-import json
-from time import sleep
+import sys
+import urllib.request
 
-bs = BeautifulScraper()
-url = "https://www.webfx.com/tools/emoji-cheat-sheet/ "
-page = bs.go(url)
-count = 0
-collection_path = '../A06/emoji_collection'
-for emoji in page.find_all("span",{"class":"emoji"}):
-    image_path = emoji['data-src']
-    print(url+image_path)
-    count += 1
-print(count)
+def collect_emoji(storage):
+    '''
+    This function collects photos from webfx.com/tools/emoji-cheat-sheet.
+    :param storage: location for emojis to be stored
+    :return: 1 if successful, 0 otherwise
+    '''
+    bs = BeautifulScraper()
+    url = "https://www.webfx.com/tools/emoji-cheat-sheet/"
+
+    page = bs.go(url)
+    if(page):
+        count = 0
+        print("Beginning Scrape.")
+        for emoji in page.find_all("span",{"class":"emoji"}):
+            image_path = emoji['data-src']
+            a,b,c = image_path.split("/")
+            urllib.request.urlretrieve(url+image_path, storage + c)
+
+def zip_emojis(directory):
+    '''
+    This function will turn the contents of a file into a zip folder.
+    The name of the zip file will be stored as emojis.zip
+    :param directory: location of directory to turn into a zip file
+    :return:
+    '''
+
+    file_paths = []
+
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+    with ZipFile('emojis.zip','w') as f:
+        for file in file_paths:
+            f.write(file)
+
+if __name__ == '__main__':
+    '''
+    Example parameters:
+    python3 emoji_scraper.py /some/storage/location/
+    '''
+    if (len(sys.argv)==2):
+        # Collect all emojis and store
+        collect_emoji(sys.argv[1])
+
+        # Zip directory of stored emojis
+        zip_emojis(sys.argv[1])
+
+
+
+
